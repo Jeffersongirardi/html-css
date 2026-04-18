604e9080b4e586cf4570e8d3d43994d1e08b4013
Jeffersongirardi <157550626+Jeffersongirardi@users.noreply.github.com>
2026-04-18 16:17:19 -0300

    Aula node and angular
    Aula node and angular

---
+++ .idea/UNINTER FRONTEND/index.js
@@ -0,0 +1,10 @@
+var http = require("http");
+
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/json" });
+    res.end(JSON.stringify({ numero: 1, name: "UNINTER FRONTEND" }));
+  })
+  .listen(8080);
+
+console.log("Server running at http://127.0.0.1:8080/");
